2b6ef740c8506817c9b4b810faa0e4934a6d0502
Yunseo Hwang <yunseo.h68@gmail.com>
2020-07-11 20:23:54 +0900

    Fix : Fix braillepatterndots::bpd
    Fix braillepatterndots::bpd::get_code
    Refactor braillepatterndots::bpd::get_char

--- tests/braille.rs
+++ tests/braille.rs
@@ -13,54 +13,103 @@ fn get_char_test() {
 }
 
 #[test]
+fn get_char_invalid_parameter_test() {
+    assert_eq!(bpd::get_char(0914211121), ' ');
+    assert_eq!(bpd::get_char(1234567890), ' ');
+    assert_eq!(bpd::get_char(99), ' ');
+    assert_eq!(bpd::get_char(99312), ' ');
+}
+
+#[test]
 fn get_code_test() {
-    let braille_code = bpd::get_code(0);
+    let mut braille_code = bpd::get_code(0);
     assert_eq!(braille_code[0], false);
     assert_eq!(braille_code[1], false);
     assert_eq!(braille_code[2], false);
     assert_eq!(braille_code[3], false);
     assert_eq!(braille_code[4], false);
     assert_eq!(braille_code[5], false);
-    let braille_code = bpd::get_code(1);
+
+    braille_code = bpd::get_code(1);
     assert_eq!(braille_code[0], true);
     assert_eq!(braille_code[1], false);
     assert_eq!(braille_code[2], false);
     assert_eq!(braille_code[3], false);
     assert_eq!(braille_code[4], false);
     assert_eq!(braille_code[5], false);
-    let braille_code = bpd::get_code(12);
+
+    braille_code = bpd::get_code(12);
     assert_eq!(braille_code[0], true);
     assert_eq!(braille_code[1], true);
     assert_eq!(braille_code[2], false);
     assert_eq!(braille_code[3], false);
     assert_eq!(braille_code[4], false);
     assert_eq!(braille_code[5], false);
-    let braille_code = bpd::get_code(14);
+
+    braille_code = bpd::get_code(14);
     assert_eq!(braille_code[0], true);
     assert_eq!(braille_code[1], false);
     assert_eq!(braille_code[2], false);
     assert_eq!(braille_code[3], true);
     assert_eq!(braille_code[4], false);
     assert_eq!(braille_code[5], false);
-    let braille_code = bpd::get_code(145);
+
+    braille_code = bpd::get_code(145);
     assert_eq!(braille_code[0], true);
     assert_eq!(braille_code[1], false);
     assert_eq!(braille_code[2], false);
     assert_eq!(braille_code[3], true);
     assert_eq!(braille_code[4], true);
     assert_eq!(braille_code[5], false);
-    let braille_code = bpd::get_code(15);
+
+    braille_code = bpd::get_code(15);
     assert_eq!(braille_code[0], true);
     assert_eq!(braille_code[1], false);
     assert_eq!(braille_code[2], false);
     assert_eq!(braille_code[3], false);
     assert_eq!(braille_code[4], true);
     assert_eq!(braille_code[5], false);
-    let braille_code = bpd::get_code(124);
+
+    braille_code = bpd::get_code(124);
     assert_eq!(braille_code[0], true);
     assert_eq!(braille_code[1], true);
     assert_eq!(braille_code[2], false);
     assert_eq!(braille_code[3], true);
+    assert_eq!(braille_code[4], false);
+    assert_eq!(braille_code[5], false);
+}
+
+#[test]
+fn get_code_invalid_parameter_test() {
+    let mut braille_code = bpd::get_code(0914211121);
+    assert_eq!(braille_code[0], false);
+    assert_eq!(braille_code[1], false);
+    assert_eq!(braille_code[2], false);
+    assert_eq!(braille_code[3], false);
+    assert_eq!(braille_code[4], false);
+    assert_eq!(braille_code[5], false);
+
+    braille_code = bpd::get_code(1234567890);
+    assert_eq!(braille_code[0], false);
+    assert_eq!(braille_code[1], false);
+    assert_eq!(braille_code[2], false);
+    assert_eq!(braille_code[3], false);
+    assert_eq!(braille_code[4], false);
+    assert_eq!(braille_code[5], false);
+
+    braille_code = bpd::get_code(99);
+    assert_eq!(braille_code[0], false);
+    assert_eq!(braille_code[1], false);
+    assert_eq!(braille_code[2], false);
+    assert_eq!(braille_code[3], false);
+    assert_eq!(braille_code[4], false);
+    assert_eq!(braille_code[5], false);
+
+    braille_code = bpd::get_code(99312);
+    assert_eq!(braille_code[0], false);
+    assert_eq!(braille_code[1], false);
+    assert_eq!(braille_code[2], false);
+    assert_eq!(braille_code[3], false);
     assert_eq!(braille_code[4], false);
     assert_eq!(braille_code[5], false);
 }
